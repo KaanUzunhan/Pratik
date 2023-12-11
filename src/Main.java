2bba0563fa29f5f943e4e81226847f3342fbe083
kaan <kaanuzunhan111@hotmail.com>
2023-12-11 20:48:13 +0300

    pratikler

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+// Press Shift tclass Main {
+//    public static void main(String[] args) {
+//        // Kodlama1.main(args);
+//        // Kodlama2.main(args);
+//        Kodlama3.main(args);
+//        //  Kodlama4.main(args);
+//
+//    }
+//}
+//class Kodlama1 {
+//    public static void main(String[] args) {
+//        // İlk Ödev ekrana Patika.dev FullStack Java Spring boot & React Bootcamp
+//        System.out.println("Patika.dev FullStack Java Spring boot & React Bootcamp");
+//    }
+//}
+//
+////Durmak yok Devam :))
+//
+//class Kodlama2 {
+//    public static void main(String[] args) {
+//        // 600 e kadar saydıracağız
+//        for (int x=0; x<=600; x++){
+//
+//
+//        }
+//    }
+//}
+//
+//class Kodlama3 {
+//    public static void main(String[] args) {
+//        // 600 sayısının 2 ve 3 katlarını alacağız
+//        for (int x=1; x <=600; x++){
+//
+//            if ( x % 2 == 0){ // sayı 2 nin katı ise
+//                System.out.println(x +" 2'ün katıdır.");
+//            }
+//            if ( x % 3 == 0){ // sayı 3 ün katı ise
+//                System.out.println(x +" 3'ün katıdır.");
+//            }
+//        }
+//    }
+//}
+//
+//class Kodlama4 {
+//    public static void main(String[] args) { //2-200 kadar  olan cift sayıları yazdır
+//        System.out.println("2'den 200'e kadar olan çift sayılar:");
+//
+//        for (int i = 2; i <= 200; i += 2) {
+//            System.out.println(i);
+//        }
+//    }
+//}
